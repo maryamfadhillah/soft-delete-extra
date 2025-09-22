ef0eb7bf36d9a7259365f54da45d6f27aa1a46eb
Mayam Fadhillah <57205595+maryamfadhillah@users.noreply.github.com>
2025-09-22 08:44:44 +0700

    Update SoftDeleteExtra.php
    eliminate deleted_at, only using is_deleted for softdelete indicator

--- src/Traits/SoftDeleteExtra.php
+++ src/Traits/SoftDeleteExtra.php
@@ -1,26 +1,82 @@
 <?php
 
-namespace SoftDeleteExtra\Traits;
+namespace App\Traits;
 
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 
-trait SoftDeleteExtra
+trait CustomSoftDelete
 {
-    use SoftDeletes;
-
-    protected static function bootSoftDeleteExtra()
+    /**
+     * Boot the trait.
+     */
+    public static function bootCustomSoftDelete()
     {
-        static::deleting(function ($model) {
-            if (! $model->isForceDeleting()) {
-                if ($model->isFillable('deleted_by')) {
-                    $model->deleted_by = Auth::id() ?? 'SYSTEM';
-                }
-                if ($model->isFillable('is_deleted')) {
-                    $model->is_deleted = 1;
-                }
-                $model->save();
-            }
+        static::addGlobalScope('is_deleted', function (Builder $builder) {
+            $builder->where($builder->getModel()->getQualifiedIsDeletedColumn(), false);
         });
+    }
+
+    /**
+     * Get the fully qualified "is_deleted" column.
+     */
+    public function getQualifiedIsDeletedColumn()
+    {
+        return $this->getTable().'.'.$this->getIsDeletedColumn();
+    }
+
+    /**
+     * Get the "is_deleted" column name.
+     */
+    public function getIsDeletedColumn()
+    {
+        return defined('static::IS_DELETED') ? static::IS_DELETED : 'is_deleted';
+    }
+
+    /**
+     * Perform the delete operation (soft delete).
+     */
+    protected function performDeleteOnModel()
+    {
+        if ($this->exists) {
+            $this->{$this->getIsDeletedColumn()} = true;
+            $this->deleted_by = auth()->id() ?: 'SYSTEM';
+            $this->save();
+        }
+    }
+
+    /**
+     * Restore a soft-deleted model instance.
+     */
+    public function restore()
+    {
+        $this->{$this->getIsDeletedColumn()} = false;
+        $this->deleted_by = NULL;
+        $this->save();
+
+        return $this;
+    }
+
+    /**
+     * Force delete (hard delete).
+     */
+    public function forceDelete()
+    {
+        return parent::delete();
+    }
+
+    /**
+     * Include soft-deleted records in query.
+     */
+    public static function withTrashed()
+    {
+        return (new static)->newQueryWithoutScope('is_deleted');
+    }
+
+    /**
+     * Only soft-deleted records.
+     */
+    public static function onlyTrashed()
+    {
+        return (new static)->withTrashed()->where((new static)->getIsDeletedColumn(), true);
     }
 }
